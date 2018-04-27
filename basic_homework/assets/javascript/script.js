6a6bb0031aec1152d221a69a09a00166b68f8e16
unknown <Joseph.Kampa@gmail.com>
2018-04-27 10:23:19 -0500

    MomentJS, CleanUp of Code

--- basic_homework/assets/javascript/script.js
+++ basic_homework/assets/javascript/script.js
@@ -6,17 +6,17 @@ var config = {
     projectId: "homework-week-7",
     storageBucket: "homework-week-7.appspot.com",
     messagingSenderId: "623924369382"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  //Variable
-  var database = firebase.database();
-  var trainName = '';
-  var trainDestination = '';
-  var trainTime = '';
-  var trainFrequency = '';
+//Variable
+var database = firebase.database();
+var trainName = '';
+var trainDestination = '';
+var trainTime = '';
+var trainFrequency = '';
 
-  $("#addTrain").on("click", function (event) {
+$("#addTrain").on("click", function (event) {
 
     event.preventDefault();
 
@@ -37,25 +37,34 @@ var config = {
 database.ref().on("child_added", function (childSnapshot, prevChildKey) {
 
     var trainName = childSnapshot.val().name;
-  var trainDestination = childSnapshot.val().destination;
-  var trainTime = childSnapshot.val().time;
-  var trainFrequency = childSnapshot.val().frequency;
+    var trainDestination = childSnapshot.val().destination;
+    var trainTime = childSnapshot.val().time;
+    var trainFrequency = childSnapshot.val().frequency;
 
     console.log(trainName);
     console.log(trainDestination);
     console.log(trainTime);
     console.log(trainFrequency);
 
+    var trainTimeConverted = moment(trainTime, "HH:mm").subtract(1, "years");
+    var currentTime = moment();
+    var diffTime = moment().diff(moment(trainTimeConverted), "minutes");
+    var timeRemainder = diffTime % trainFrequency;
+    var minutesTillTrain = trainFrequency - timeRemainder;
+    var nextTrain = moment().add(minutesTillTrain, "minutes");
 
-   
+
     $("#trainSchedule").append("<tr>" +
         "<td>" + trainName + "</td>" +
         "<td>" + trainDestination + "</td>" +
         "<td>" + trainFrequency + "</td>" +
-        "<td> </td>" +
-        "<td> </td>" +
+        "<td>" + moment(nextTrain).format("hh:mm A") + "</td>" +
+        "<td>" + minutesTillTrain + "</td>" +
         "</tr>");
 
 }, function (errorObject) {
     console.log("Errors handled: " + errorObject.code);
 });
+
+
+$("#currentTime").html(moment().format("hh:mm A"));
